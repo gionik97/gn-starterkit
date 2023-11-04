a94c6d548e30f4f35fdb9b2f2713ea6fab407348
gionik97 <45343354+gionik97@users.noreply.github.com>
2023-11-04 21:19:21 +0400

    Feature ( modules - Search ) - developing search functionality for posts

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,11 @@ import "../sass/main.scss";
 // Our modules / classes
 import MobileMenu from "./modules/MobileMenu";
 import HeroSlider from "./modules/HeroSlider";
+import Search from "./modules/Search";
 
 // Instantiate a new object using our modules/classes
-// const mobileMenu = new MobileMenu();
+const mobileMenu = new MobileMenu();
+const search = new Search();
 // const heroSlider = new HeroSlider();
 
 console.log("HELLO WORDPRESS WORLD yo");
