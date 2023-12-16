c8090a4742315fbbaf897e56506055a55cb2aee8
gionik97 <45343354+gionik97@users.noreply.github.com>
2023-12-16 14:55:38 +0400

    Feature ( modules - Search ) - anhanced search feature

--- src/js/modules/Search.js
+++ src/js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from "jquery";
 class Search {
   // 1 describe and create/initiate our object
   constructor() {
+    this.addSearchHTML();
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
@@ -33,7 +34,7 @@ class Search {
           this.resultsDiv.html('<div class="spinner-loader"></div>');
           this.isSpinnerVisible = true;
         }
-        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750);
       } else {
         this.resultsDiv.html("");
         this.isSpinnerVisible = false;
@@ -43,22 +44,44 @@ class Search {
   }
 
   getResults() {
-    $.getJSON(
-      "http://gn-starter.local/wp-json/wp/v2/posts?search=" +
-        this.searchField.val(),
-      (posts) => {
+    $.when(
+      $.getJSON(
+        starterkitData.root_url +
+          "/wp-json/wp/v2/posts?search=" +
+          this.searchField.val()
+      ),
+      $.getJSON(
+        starterkitData.root_url +
+          "/wp-json/wp/v2/pages?search=" +
+          this.searchField.val()
+      )
+    ).then(
+      (posts, pages) => {
+        var combineResults = posts[0].concat(pages[0]);
         this.resultsDiv.html(`
-          <h2 class="search-overlay__section-title">General Information</h2>
-          <ul class="link-list min-list">
-            ${posts
-              .map((item) => {
-                return `<li><a href"${item.link}">${item.title.rendered}</a></li>`;
-              })
-              .join(" ")}
-          </ul>
-        `);
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${
+          combineResults.length
+            ? '<ul class="link-list min-list">'
+            : "<p>No General Information matches this search.</p>"
+        }
+    
+        ${combineResults
+          .map((item) => {
+            return `<li><a href"${item.link}">${item.title.rendered}</a> ${
+              item.type == "post" ? `by: ${item.authorName}` : ""
+            }</li>`;
+          })
+          .join(" ")}
+          ${combineResults.length ? "</ul>" : ""}
+      `);
+        this.isSpinnerVisible = false;
+      },
+      () => {
+        this.resultsDiv.html(`<p>Unexpected error; please try again.</p>`);
       }
     );
+
     // this.isSpinnerVisible = false;
   }
 
@@ -78,6 +101,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val(" ");
+    setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
     console.log("open method just ran");
   }
@@ -87,6 +112,27 @@ class Search {
     $("body").removeClass("body-no-scroll");
     this.isOverlayOpen = false;
     console.log("close method just ran");
+  }
+
+  addSearchHTML() {
+    $("body").append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            
+            <input type="text" class="search-term" id="search-term" placeholder="What are you looking for?" />
+
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+
+          </div>
+        </div>
+        <div class="container">
+            <div id="search-overlay__results"></div>
+        </div>
+      </div>
+    `);
   }
 }
 
